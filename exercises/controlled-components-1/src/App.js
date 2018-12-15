36ec5719db2affd69317ea2e395e3cfc6e29440a
castrodrigo <castrodrigo@yahoo.com.br>
2018-12-15 23:38:58 +0100

    Add InputMirror controlled component to handle the work

--- exercises/controlled-components-1/src/App.js
+++ exercises/controlled-components-1/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import InputMirror from "./components/InputMirror";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -11,9 +12,7 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
-          <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <InputMirror />
         </div>
       </div>
     );
