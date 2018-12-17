53ff621fc9545ad45e5721830059a12f623b56d5
castrodrigo <castrodrigo@yahoo.com.br>
2018-12-17 23:22:57 +0100

    Convert component to Stateful in order to handle rendering behavior

--- exercises/all-together-1/src/components/ListUsers.js
+++ exercises/all-together-1/src/components/ListUsers.js
@@ -1,25 +1,40 @@
 import React from "react";
 
-const ListUsers = ({ users }) => (
-  <React.Fragment>
-    <h2 className="users">Users</h2>
-    <table>
-      <thead>
-        <tr key="header">
-          <th>Username</th>
-          <th>Total Games</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map(item => (
-          <tr key={item.userName}>
-            <td>{item.userName}</td>
-            <td>{item.totalGames}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </React.Fragment>
-);
+class ListUsers extends React.Component {
+  state = {
+    show: true
+  };
+
+  toogleGames = () => {
+    this.setState(prevState => ({ show: !prevState.show }));
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h2 className="users">Users</h2>
+        <button onClick={this.toogleGames}>
+          {this.state.show ? "Hide" : "Show"} total games played
+        </button>
+        <table>
+          <thead>
+            <tr key="header">
+              <th>Username</th>
+              <th>Total Games</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.users.map(item => (
+              <tr key={item.userName}>
+                <td>{item.userName}</td>
+                <td>Played {this.state.show ? item.totalGames : "*"} games</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
 
 export default ListUsers;
